6d951ff1e569771406191834c105293a7a1117fb
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-09-24 18:16:12 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan1/biodata/IF110118021Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan1.biodata;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program :Biodata
+ */
+public class IF110118021Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA SAYA" + "\n" + "------------");
+        System.out.println("NAMA\t\t: M Arvan Nurcahyana");
+        System.out.println("ALAMAT\t\t: Antapani");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR \t: 19 Agustus 2000");
+        System.out.println("------------");
+        System.out.println("HOBI\t\t: Tidur");
+        System.out.println("MAKES\t\t: Nasgor");
+        System.out.println("MIKES\t\t: Minuman dingin");
+    }
+    
+}
